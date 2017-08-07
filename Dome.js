690849d73596a5fff516c128f1f7cd0d0209f733
Paul Murray <pmurray@bigpond.com>
2017-08-07 16:29:10 +1000

    Success at building controller objects.

--- Dome.js
+++ Dome.js
@@ -3,4 +3,39 @@ console.log("Dome.js start");
 
 
 
+$(function() {
+  console.log("initialization");
+  
+});
+
+function ctl(e) {
+  while(e) {
+    if(e.controller) return e.controller;
+    e = e.parentNode;
+  }
+  return null;
+}
+
+function addLayer() {
+  console.log("add layer");
+  
+  var layer = $(".layer.template").clone(true, true);
+  layer.removeClass("template");
+  layer = layer[0];
+  layer.controller = new Layer(layer);
+  
+  $("#layers").prepend(layer);
+}
+
+var layeridx = 1;
+function Layer(element) {
+  this.idx = layeridx++;
+  this.element = element;
+}
+
+function test(t) {
+  console.log("test");
+  console.log(ctl(t).idx);
+}
+
 console.log("Dome.js ok");
